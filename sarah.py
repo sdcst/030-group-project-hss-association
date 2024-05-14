8832b1e0e5f523885eed2e48416685dadb4e402a
sar37 <158889075+sar37@users.noreply.github.com>
2024-05-14 13:15:50 -0700

    Update sarah.py

--- sarah.py
+++ sarah.py
@@ -1,3 +1,5 @@
+
+
 def arearectangle():
     l = input("enter the length ")
     w = input("enter the width ")
@@ -5,6 +7,8 @@ def arearectangle():
     w= float(w)
     a = l*w
     print(f"the area of the rectangle given is {a} ")
+
+   
 
 def areatriangle():
     b = input("enter the base")
@@ -14,4 +18,26 @@ def areatriangle():
     a = 0.5*b*h
     print(f"the area of the triangle is {a} ")
 
-    areatriangle()
+def volumecylinder():
+    import math 
+    r = input("enter radius")
+    h = input("enter in the height")
+    r = float(r)
+    h = float(h)
+    v = math.pi*r**0.5*h
+    print(f"the volume of a cylinder is {v}")
+
+    volumecylinder()
+
+def budget():
+    h = input("what is your hourly income?")
+    m = input("what is your monthly income?")
+    r = input("how much is your rent?")
+    h = float(h)
+    m = float(m)
+    r = float(r)
+    total = h + m - r
+    total = float(total)
+    print(f"the have {total} dollars to spend on groceries, phone, living needs,etc ")
+     
+    
