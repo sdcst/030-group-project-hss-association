df1688633d1f85bcab7f639d5e390ed65df84525
sar37 <158889075+sar37@users.noreply.github.com>
2024-05-14 12:38:23 -0700

    Update sarah.py

---
+++ sarah.py
@@ -0,0 +1,17 @@
+def arearectangle():
+    l = input("enter the length ")
+    w = input("enter the width ")
+    l = float(l)
+    w= float(w)
+    a = l*w
+    print(f"the area of the rectangle given is {a} ")
+
+def areatriangle():
+    b = input("enter the base")
+    h = input("enter the height")
+    b = float(b)
+    h = float(h)
+    a = 0.5*b*h
+    print(f"the area of the triangle is {a} ")
+
+    areatriangle()
