385cd6a35df929d4918addd445cdde29df9641ea
sar37 <158889075+sar37@users.noreply.github.com>
2024-05-14 13:29:04 -0700

    Update sarah.py

--- sarah.py
+++ sarah.py
@@ -27,17 +27,50 @@ def volumecylinder():
     v = math.pi*r**0.5*h
     print(f"the volume of a cylinder is {v}")
 
-    volumecylinder()
+  
 
 def budget():
     h = input("what is your hourly income?")
     m = input("what is your monthly income?")
-    r = input("how much is your rent?")
+    r = input("how much is your monthly rent?")
     h = float(h)
     m = float(m)
     r = float(r)
-    total = h + m - r
+    total = m - r
     total = float(total)
-    print(f"the have {total} dollars to spend on groceries, phone, living needs,etc ")
+    print(f"the have {total} dollars to spend on groceries, phone, living needs,savings, etc per month ")
+
+def numberguessing():
+    l = input("pick a level: hard ,medium or easy")
+    if l == "easy":
+        print("pick a number between 1 - 10")
+        n = 2
+        n = float(n)
+        for n in range(1,10):
+            if n == "2":
+                print("you got it!")
+            else:
+                print("not quite")
+
+    elif l == "medium":
+        print("pick a number between 1-50")
+        nn = 10
+        nn = float(nn)
+        for nn in range(1,50):
+            if nn == "10":
+                print("you got it!")
+            else:
+                print("not quite")
+
+                
+    elif l == "hard":
+        print("pick a number between 1-100")
+        nnn = 57
+        nnn = float(nnn)
+        for nnn in range(1,100):
+            if nnn == "57":
+                print("you got it!")
+            else:
+                print("not quite")
      
     
